908b643f288556dafcc01c908e9fe5a5cd5a46e6
frt-jpeg <frt-jpeg@163.com>
2026-05-27 20:02:17 +0800

    picture

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "FRT's Blog";
-export const SITE_DESCRIPTION = "my blog";
+export const SITE_TITLE = "FRT's HomeSit";
+export const SITE_DESCRIPTION = "My HomeSit";
